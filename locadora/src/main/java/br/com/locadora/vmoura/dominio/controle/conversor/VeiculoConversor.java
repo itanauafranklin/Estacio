657e61590010d8c57b0d55e4a0c4bee046e02fb0
itanauafranklin <itanaua@gmail.com>
2017-11-18 21:45:19 -0300

    conversor veículo

---
+++ locadora/src/main/java/br/com/locadora/vmoura/dominio/controle/conversor/VeiculoConversor.java
@@ -0,0 +1,7 @@
+package br.com.locadora.vmoura.dominio.controle.conversor;
+
+import br.com.locadora.vmoura.dominio.entidade.Veiculo;
+
+public class VeiculoConversor extends ObjetoPersistenteConversor<Veiculo> {
+
+}
