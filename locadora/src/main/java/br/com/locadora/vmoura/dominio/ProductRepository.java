cde16b92e766977b609fd3e2e8e2e497b6b5d5fd
itanauafranklin <itanaua@gmail.com>
2017-11-26 21:24:32 -0300

    removendo arquivos não utilizados

--- locadora/src/main/java/br/com/locadora/vmoura/dominio/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.locadora.vmoura.dominio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
